cbce6b23ea24580354aca7c338d7f96bad0bf4d8
jesus quesada <jesus.quesada@bitsamericas.com>
2022-02-28 10:27:50 -0500

    ApiUrl for .net api added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000",
+  apiUrl: "https://localhost:5001",
+  apiUrlNodeJs: "http://localhost:3000",
+  apiUrlNet: "https://localhost:5001",
 };
 
 /*
